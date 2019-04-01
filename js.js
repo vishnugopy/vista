707ed7047e67548a7054de3a717a6b2d9e754d8b
vishnugopy <vishnugoby0010@gmail.com>
2019-04-01 10:06:04 +0200

    hi

---
+++ js.js
@@ -0,0 +1,31 @@
+var boys = [
+  '<a href="https://www.instagram.com/instagram/">vishnu</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu1</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu2</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu3</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu4</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu5</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu6</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu7</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu8</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu9</a>',
+  '<a href="https://www.instagram.com/instagram/">vishnu10</a>'
+];
+
+
+
+
+var girls = [
+  '<a href="https://www.instagram.com/instagram/">gopy</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy1</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy2</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy3</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy4</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy5</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy6</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy7</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy8</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy9</a>',
+  '<a href="https://www.instagram.com/instagram/">gopy10</a>'
+
+];
