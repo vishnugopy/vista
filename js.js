6949c1a62b6587d1fc0c2bef15026ee4f7cf552b
vishnugopy <vishnugoby0010@gmail.com>
2019-04-03 14:21:00 +0200

    0.1.1.3

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var boys = [
-'<a href="https://www.instagram.com/ajan_thamizhte/"> @ajan_thamizhte </a>',
-'<a href="https://www.instagram.com/___my_name_is_billa___/"> @___my_name_is_billa___ </a>'
+'<a href="https://www.instagram.com/ajan_thamizhte/" target="_blank"> @ajan_thamizhte </a>',
+'<a href="https://www.instagram.com/___my_name_is_billa___/" target="_blank"> @___my_name_is_billa___ </a>'
   
 
 ];
@@ -9,15 +9,15 @@ var boys = [
 
 
 var girls = [
-'<a href=" https://www.instagram.com/__gowshi__10/"> @__gowshi__10</a>',
-'<a href="https://www.instagram.com/mathu.ka_slk/"> @mathu.ka_slk </a>',
-'<a href="https://www.instagram.com/shasha__gurl/"> @shasha__gurl </a>',
-'<a href="https://www.instagram.com/axngxixe/"> @axngxixe </a>',
-'<a href="https://www.instagram.com/suru_1605/"> @suru_1605 </a>',
-'<a href="https://www.instagram.com/dilo.slk/"> @Dilo.slk </a>',
-'<a href="https://www.instagram.com/pirya2267/"> @Pirya2267 </a>',
-'<a href="https://www.instagram.com/lea_segar/"> @lea_segar </a>',
-'<a href="https://www.instagram.com/sweetyannie1999/"> @Sweetyannie1999 </a>',
+'<a href=" https://www.instagram.com/__gowshi__10/" target="_blank"> @__gowshi__10</a>',
+'<a href="https://www.instagram.com/mathu.ka_slk/" target="_blank"> @mathu.ka_slk </a>',
+'<a href="https://www.instagram.com/shasha__gurl/" target="_blank"> @shasha__gurl </a>',
+'<a href="https://www.instagram.com/axngxixe/" target="_blank"> @axngxixe </a>',
+'<a href="https://www.instagram.com/suru_1605/" target="_blank"> @suru_1605 </a>',
+'<a href="https://www.instagram.com/dilo.slk/" target="_blank"> @Dilo.slk </a>',
+'<a href="https://www.instagram.com/pirya2267/" target="_blank"> @Pirya2267 </a>',
+'<a href="https://www.instagram.com/lea_segar/" target="_blank"> @lea_segar </a>',
+'<a href="https://www.instagram.com/sweetyannie1999/" target="_blank"> @Sweetyannie1999 </a>',
 
 ];
 
